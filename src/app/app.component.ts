7a0ae13de811014fbbb23c7b0498b72ebd70a07d
SaidAoussar <aoussarsaid33@gmail.com>
2023-05-30 20:52:46 +0100

    feat: create divider block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blocks-app';
+  size = 1;
+  color = "#121fd9"
+  dividerWithText = false;
+  dividerText="test";
+  dividerAlignText="center";
+
+
+  onChangeSize(e:any){
+    this.size = e.target.value;
+  }
+  onChangeColor(e:any){
+    this.color = e.target.value;
+  }
+
+  toggleText(){
+    this.dividerWithText = !this.dividerWithText;
+  }
+
+  onChangeText(e:any){
+    this.dividerText = e.target.value;
+  }
+  onChangeAlignText(e:any){
+    this.dividerAlignText = e.target.value
+  }
+
 }
