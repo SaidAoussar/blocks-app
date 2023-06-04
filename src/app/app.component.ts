13107e942e137e991d55db2a80a01374609671a3
SaidAoussar <aoussarsaid33@gmail.com>
2023-06-04 22:39:58 +0100

    refactor: switch form to material angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,15 @@ export class AppComponent {
   size = 1;
   color = "#121fd9"
   dividerWithText = false;
-  dividerText="test";
-  dividerAlignText="center";
   dividerWidth = "100";
   dividerWidthUnit = "%";
   dividerPosition = "start";
+  dividerText="divider";
   textColor="#000000";
   textSize = 14;
+  textFontWeight = 400;
   textFontFamily = '"Lucida Console", "Courier New", monospace';
+  textAlign="center";
 
   onChangeSizeValue(value:any){
     if(typeof value == 'number'){
@@ -25,26 +26,18 @@ export class AppComponent {
       this.size = 1;
     }
   }
-
-
   onChangeWidth(value:any){
     this.dividerWidth = value;
   }
   onChangeWidthUnit(value:any){
     this.dividerWidthUnit = value;
   }
-
   onChangePosition(e:any){
     this.dividerPosition = e.target.value;
   }
-
   onChangeColor(e:any){
     this.color = e.target.value;
   }
-  toggleText(){
-    this.dividerWithText = !this.dividerWithText;
-  }
-
   onChangeText(value:any){
     this.dividerText = value;
   }
@@ -52,19 +45,12 @@ export class AppComponent {
   onChangeTextSize(e:any){
     if(!Number.isNaN(parseFloat(e.target.value))){
       this.textSize = e.target.value;
-      console.log("hello from number")
     }else{
-      console.log("not number")
-
       this.textSize = 14;
     }
   }
-
   onChangeTextFontFamily(e:any){
     this.textFontFamily = e.target.value;
-  }
-  onChangeAlignText(e:any){
-    this.dividerAlignText = e.target.value;
   }
   onChangeTextColor(e:any){
     this.textColor = e.target.value;
