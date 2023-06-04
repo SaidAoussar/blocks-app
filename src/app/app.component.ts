571249d2c553a19f70fec0be9862bf31a9a90289
SaidAoussar <aoussarsaid33@gmail.com>
2023-06-04 17:35:40 +0100

    feat: add more parameter to divider block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,57 @@ export class AppComponent {
   dividerWithText = false;
   dividerText="test";
   dividerAlignText="center";
-  textColor="#000000"
+  dividerWidth = "100";
+  dividerWidthUnit = "%";
+  dividerPosition = "start";
+  textColor="#000000";
+  textSize = 14;
+  textFontFamily = '"Lucida Console", "Courier New", monospace';
 
-
-  onChangeSize(e:any){
-    this.size = e.target.value;
+  onChangeSizeValue(value:any){
+    if(typeof value == 'number'){
+      this.size=value;
+    }else{
+      this.size = 1;
+    }
   }
+
+
+  onChangeWidth(value:any){
+    this.dividerWidth = value;
+  }
+  onChangeWidthUnit(value:any){
+    this.dividerWidthUnit = value;
+  }
+
+  onChangePosition(e:any){
+    this.dividerPosition = e.target.value;
+  }
+
   onChangeColor(e:any){
     this.color = e.target.value;
   }
-
   toggleText(){
     this.dividerWithText = !this.dividerWithText;
   }
 
   onChangeText(value:any){
     this.dividerText = value;
+  }
+
+  onChangeTextSize(e:any){
+    if(!Number.isNaN(parseFloat(e.target.value))){
+      this.textSize = e.target.value;
+      console.log("hello from number")
+    }else{
+      console.log("not number")
+
+      this.textSize = 14;
+    }
+  }
+
+  onChangeTextFontFamily(e:any){
+    this.textFontFamily = e.target.value;
   }
   onChangeAlignText(e:any){
     this.dividerAlignText = e.target.value;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button"
+import {MatRadioModule} from "@angular/material/radio"
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { TabsComponent } from './blocks/tabs/tabs.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
